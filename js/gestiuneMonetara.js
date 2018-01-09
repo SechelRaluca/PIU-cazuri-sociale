6f7c916fe2ef70808608580f02587c0a09361abb
Lenuta-Denisa Iuga <lenutadenisaiuga@gmail.com>
2018-01-09 17:29:10 +0200

    Refactor monetary management for NGO members page

--- js/gestiuneMonetara.js
+++ js/gestiuneMonetara.js
@@ -1,38 +1,42 @@
-function validateSum(sum){
-	if(isNaN(sum.value)) {
-		sum.style.backgroundColor = "#FF0000";
-		sum.focus();
+function checkSumInput(sumaCazDeAlocat, sumaCazDejaAlocata, buton, popup){
+	var sumaTotala = parseInt(document.getElementById('suma').innerHTML, 10);
+	var sumaAlocataCaz = parseInt(document.getElementById(sumaCazDeAlocat).value, 10);
+	
+	if(isNaN(parseInt(document.getElementById(sumaCazDeAlocat).value, 10))) {
+		toastr.error('Câmpul numeric trebuie completat!');
+		return false;
+	}
+	
+	if(sumaAlocataCaz <= 0){
+		toastr.error('Introduceţi o valoare pozitivă nenulă!');
+		document.getElementById(sumaCazDeAlocat).value = "";
+		return false;
+	}
+	
+	if(sumaTotala < sumaAlocataCaz) {
+		toastr.error('Nu sunt suficienţi bani în cont!');
+		document.getElementById(sumaCazDeAlocat).value = "";
 		return false;
 	}else{
-		sum.style.backgroundColor = "#FFFFFF";
+		$(buton).attr('data-target', popup);
 		return true;
 	}
 }
 
-function validateSumInput(sumaCaz){
-	var sum = document.getElementById(sumaCaz);
-	sum.onchange = function(){
-		validateSum(sum);
-	}
+function disablePopup(buton) {
+	$(buton).attr('data-target', "");
 }
 
 function send(sumaCazDeAlocat, sumaCazDejaAlocata) {
 	var sumaTotala = parseInt(document.getElementById('suma').innerHTML, 10);
 	var sumaAlocataCaz = parseInt(document.getElementById(sumaCazDeAlocat).value, 10);
 	
-	if(sumaTotala - sumaAlocataCaz < 0) {
-		toastr.error('Nu sunt suficienţi bani în cont!');
-		document.getElementById(sumaCazDeAlocat).value = "";
-		return false;
-	}else {
-		var sumaTotalaAlocataCaz = sumaAlocataCaz;
-		if(!isNaN(parseInt(document.getElementById(sumaCazDejaAlocata).innerHTML, 10))) {
-			sumaTotalaAlocataCaz += parseInt(document.getElementById(sumaCazDejaAlocata).innerHTML, 10);
-		}
-		document.getElementById(sumaCazDejaAlocata).innerHTML = sumaTotalaAlocataCaz;
-		document.getElementById('suma').innerHTML = sumaTotala - sumaAlocataCaz;
-		document.getElementById(sumaCazDeAlocat).value = "";
-		toastr.info('Repartizare realizată cu succes!');
-		return true;
-	}
+	var sumaTotalaAlocataCaz = sumaAlocataCaz;
+	sumaTotalaAlocataCaz += parseInt(document.getElementById(sumaCazDejaAlocata).innerHTML, 10);
+	document.getElementById(sumaCazDejaAlocata).innerHTML = sumaTotalaAlocataCaz;
+	document.getElementById('suma').innerHTML = sumaTotala - sumaAlocataCaz;
+	document.getElementById(sumaCazDeAlocat).value = "";
+	toastr.info('Repartizare realizată cu succes!');
+	
+	return true;
 }
