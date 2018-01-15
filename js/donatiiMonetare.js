4c51e04c1cbc47e8e4f33d073620c1fefde9f4de
andreeacozma <andreeacozma95@yahoo.com>
2018-01-16 01:38:17 +0200

    forms and validation for Donatii monetare, Donatii pentru un caz

--- js/donatiiMonetare.js
+++ js/donatiiMonetare.js
@@ -2,18 +2,21 @@ function validateSum(sumaDeDonat){
     var sumaDeDonatCazului = parseInt(document.getElementById(sumaDeDonat).value, 10);
 
     if(isNaN(parseInt(document.getElementById(sumaDeDonat).value, 10))) {
-        toastr.error('Câmpul numeric trebuie completat!');
-        document.getElementById(sumaDeDonat).focus();
+        //toastr.error('Câmpul numeric trebuie completat!');
+        //document.getElementById(sumaDeDonat).focus();
+		document.getElementById(sumaDeDonat).style.borderColor = "red";
         return false;
     }
 
     if(sumaDeDonatCazului <= 0){
-        toastr.error('Introduceţi o valoare pozitivă nenulă!');
+        //toastr.error('Introduceţi o valoare pozitivă nenulă!');
         document.getElementById(sumaDeDonat).value = "";
-        document.getElementById(sumaDeDonat).focus();
+        //document.getElementById(sumaDeDonat).focus();
+		document.getElementById(sumaDeDonat).style.borderColor = "red";
         return false;
     }
 
+	document.getElementById(sumaDeDonat).style.borderColor = "#d6d6d6";
     return true;
 }
 
@@ -21,15 +24,18 @@ function validateLastName(nume){
     var lastName = document.getElementById(nume);
     if(lastName.value.search(/[^a-zA-Z]+/) === -1){
         if(lastName.value.length < 3) {
-            toastr.error('Lungimea câmpului trebuie să fie mai mare sau egală cu 3!');
-            lastName.focus();
+            //toastr.error('Lungimea câmpului trebuie să fie mai mare sau egală cu 3!');
+           //lastName.focus();
+			lastName.style.borderColor = "red";
             return false;
         }else{
+			lastName.style.borderColor = "#d6d6d6";
             return true;
         }
     }else{
-        toastr.error('Câmpul trebuie să conţină doar litere!');
-        lastName.focus();
+        //toastr.error('Câmpul trebuie să conţină doar litere!');
+        //lastName.focus();
+		lastName.style.borderColor = "red";
         return false;
     }
 }
@@ -38,26 +44,31 @@ function validateFirstName(prenume){
     var firstName = document.getElementById(prenume);
     if(firstName.value.search(/[^a-zA-Z]+/) === -1){
         if(firstName.value.length < 3) {
-            toastr.error('Lungimea câmpului trebuie să fie mai mare sau egală cu 3!');
-            firstName.focus();
+            //toastr.error('Lungimea câmpului trebuie să fie mai mare sau egală cu 3!');
+            //firstName.focus();
+			firstName.style.borderColor = "red";
             return false;
         }else{
+			firstName.style.borderColor = "#d6d6d6";
             return true;
         }
     }else{
-        toastr.error('Câmpul trebuie să conţină doar litere!');
-        firstName.focus();
+        //toastr.error('Câmpul trebuie să conţină doar litere!');
+        //firstName.focus();
+		firstName.style.borderColor = "red";
         return false;
     }
 }
 
 function validatePhoneNumber(telefon){
     var phoneNumber = document.getElementById(telefon);
-    if(!/^\d{4}\-\d{6}$/.test(phoneNumber.value)){
-        toastr.error("Numărul de telefon nu respectă formatul nnnn-nnnnnn (4-6)");
-        phoneNumber.focus();
+    if(!/^\d{10}$/.test(phoneNumber.value)){
+        //toastr.error("Numărul de telefon nu respectă formatul nnnn-nnnnnn (4-6)");
+        //phoneNumber.focus();
+		phoneNumber.style.borderColor = "red";
         return false;
     }else{
+		phoneNumber.style.borderColor = "#d6d6d6";
         return true;
     }
 }
@@ -65,10 +76,12 @@ function validatePhoneNumber(telefon){
 function validateEmail(mail){
     var email = document.getElementById(mail);
     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)){
+		email.style.borderColor = "#d6d6d6";
         return true;
     }
-    toastr.error("Adresa de email nu este validă!");
-    email.focus();
+    //toastr.error("Adresa de email nu este validă!");
+    //email.focus();
+	email.style.borderColor = "#red";
     return false;
 }
 
@@ -100,11 +113,17 @@ function donate(frecventa1, frecventa2, suma, nume, prenume, telefon, email, but
     }
 
     if(flag == 1){
-        toastr.error("Verificaţi câmpurile formularului şi completaţile corespunzător pentru a efectua donaţia!");
+        //toastr.error("Verificaţi câmpurile formularului şi completaţile corespunzător pentru a efectua donaţia!");
         return false;
     }else{
-        $(buton).attr('data-target', popup);
-        $(buton).attr('data-dismiss', 'modal');
+        //$(buton).attr('data-target', popup);
+       // $(buton).attr('data-dismiss', 'modal');
+	    resetFieldsDonatePopup(frecventa1, frecventa2, suma, nume, prenume, telefon, email, buton, popup);
+        $('#donateModal1').modal('hide');
+		setTimeout(function(){
+		$('#modalPlata').modal('show');
+		}, 500);
+		
         return true;
     }
 }
@@ -113,33 +132,6 @@ function disablePopup(buton) {
     $(buton).attr('data-target', "");
     $(buton).attr('data-dismiss', "");
 }
-
-
-document.addEventListener("DOMContentLoaded", function() {
-
-    document.getElementById('numarCard').oninvalid = function(e) {
-        e.target.setCustomValidity("");
-        if (!e.target.validity.valid) {
-            e.target.setCustomValidity("Numarul cardului trebuie introdus !");
-        }
-    };
-
-    document.getElementById('dataExpirare').oninvalid = function(e) {
-        e.target.setCustomValidity("");
-        if (!e.target.validity.valid) {
-            e.target.setCustomValidity("Data trebuie introdusă și să fie în viitor!");
-        }
-    };
-
-
-    document.getElementById('numarVerificareCard').oninvalid = function(e) {
-        e.target.setCustomValidity("");
-        if (!e.target.validity.valid) {
-            e.target.setCustomValidity("CVC trebuie introdus !");
-        }
-    };
-});
-
 
 function resetFieldsDonatePopup(frecventa1, frecventa2, suma, nume, prenume, telefon, email){
     $(frecventa1).attr('checked',false);
@@ -151,45 +143,6 @@ function resetFieldsDonatePopup(frecventa1, frecventa2, suma, nume, prenume, tel
     document.getElementById(email).value="";
 }
 
-// function validateCardNumber(numarCard){
-// 	var cardNumber = document.getElementById(numarCard);
-// 	if(!/^\d{4}\ \d{4}\ \d{4}\ \d{4}$/.test(cardNumber.value)){
-// 		toastr.error("Numărul cardului nu respectă formatul nnnnn-nnnn-nnnnn-nnnn (4-4-4-4)");
-// 		cardNumber.focus();
-// 		return false;
-// 	}else{
-// 		return true;
-// 	}
-// }
-
-// function validateCardVerificationNumber(numarVerificareCard){
-// 	var cardVerificationNumber = document.getElementById(numarVerificareCard);
-// 	if(!/^\d{3}$/.test(cardVerificationNumber.value)){
-// 		toastr.error("Numărul de verificare a cardului nu respectă formatul nnn (3)");
-// 		cardVerificationNumber.focus();
-// 		return false;
-// 	}else{
-// 		return true;
-// 	}
-// }
-
-// function isValid(expirationDate) {
-//
-// 	var cardExpirationDate = document.getElementById(expirationDate).value;
-// 	var today = new Date().toISOString().slice(0,10);
-//
-//     if (cardExpirationDate === "") {
-//         toastr.error('Introduceți o valoare pentru data expirării cardului!');
-// 		document.getElementById(expirationDate).focus();
-//         return false;
-//     } else if (cardExpirationDate < today) {
-//         toastr.error('Reverificaţi data scrisă pe card sau cardul dumneavoatră nu mai este valabil.');
-// 		document.getElementById(expirationDate).focus();
-//         return false;
-//     } else {
-//         return true;
-//     }
-// }
 
 function resetFieldsCardPopup(cardNumber, expirationDate, cardVerificationNumber){
     document.getElementById(cardNumber).value="";
@@ -197,31 +150,80 @@ function resetFieldsCardPopup(cardNumber, expirationDate, cardVerificationNumber
     document.getElementById(cardVerificationNumber).value="";
 }
 
-// function pay(cardNumber, expirationDate, cardVerificationNumber, buton) {
-// 	var flag = 0;
-//
-// 	if(!validateCardNumber(cardNumber)) {
-// 		flag = 1;
-// 	}
-//
-// 	if(!isValid(expirationDate)) {
-// 		flag = 1;
-// 	}
-//
-// 	if(!validateCardVerificationNumber(cardVerificationNumber)) {
-// 		flag = 1;
-// 	}
-//
-// 	if(flag == 1){
-// 		toastr.error("Verificaţi câmpurile formularului şi completaţile corespunzător pentru a efectua plata!");
-// 		return false;
-// 	}else{
-// 		$(buton).attr('data-dismiss', 'modal');
-// 		resetFieldsCardPopup(cardNumber, expirationDate, cardVerificationNumber);
-// 		toastr.info('Donaţie efectuată cu succes!');
-// 		return true;
-// 	}
-// }
+
+document.addEventListener("DOMContentLoaded", function() {
+
+       document.getElementById('sumCase1').oninvalid = function (e) {
+        e.target.setCustomValidity("");
+        if (!e.target.validity.valid) {
+            e.target.setCustomValidity("Suma trebuie introdusă !");
+        }
+    };
+
+    document.getElementById('lastName1').oninvalid = function (e) {
+        e.target.setCustomValidity("");
+        if (!e.target.validity.valid) {
+            e.target.setCustomValidity("Numele trebuie introdus!");
+        }
+    };
+
+
+    document.getElementById('firstName1').oninvalid = function (e) {
+        e.target.setCustomValidity("");
+        if (!e.target.validity.valid) {
+            e.target.setCustomValidity("Prenumele trebuie introdus !");
+        }
+    };
+	
+	document.getElementById('phoneNumber1').oninvalid = function (e) {
+        e.target.setCustomValidity("");
+        if (!e.target.validity.valid) {
+            e.target.setCustomValidity("Telefonul trebuie introdus și să conțină 10 cifre !");
+        }
+    };
+	
+	document.getElementById('email1').oninvalid = function (e) {
+        e.target.setCustomValidity("");
+        if (!e.target.validity.valid) {
+            e.target.setCustomValidity("Email-ul trebuie introdus !");
+        }
+    };
+	
+	document.getElementById('numarCard').oninvalid = function (e) {
+        e.target.setCustomValidity("");
+        if (!e.target.validity.valid) {
+            e.target.setCustomValidity("Numarul cardului trebuie introdus !");
+        }
+    };
+
+    document.getElementById('dataExpirare').oninvalid = function (e) {
+        e.target.setCustomValidity("");
+        if (!e.target.validity.valid) {
+            e.target.setCustomValidity("Data trebuie introdusă și să fie în viitor!");
+        }
+    };
+
+
+    document.getElementById('numarVerificareCard').oninvalid = function (e) {
+        e.target.setCustomValidity("");
+        if (!e.target.validity.valid) {
+            e.target.setCustomValidity("CVC trebuie introdus !");
+        }
+    };
+	
+	$('#donateModal1').on('hidden.bs.modal', 
+	function () { 
+		$(this).data('bs.modal', null); 
+		$('#donateModal1').removeClass('fade');
+	});
+	$('#modalPlata').on('hide.bs.modal', 
+	function () { 
+		$(this).data('bs.modal', null); 
+		$('#modalPlata').removeClass('fade');
+	});
+});
+
+
 
 function platesteComanda(){
 
@@ -251,7 +253,6 @@ function platesteComanda(){
 
     if (valid)
     {
-        $('#payModal1').modal('hide');
         toastr.info('Plată efectuată cu succes!');
     }
 
