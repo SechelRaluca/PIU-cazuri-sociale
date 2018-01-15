27a8e87ed47f1b0758bd95b8776ed45b9b028cf6
Sechel Raluca <ralucarodicasechel@gmail.com>
2018-01-15 17:39:24 +0200

    Final refactor

--- js/adaugaCazSocial.js
+++ js/adaugaCazSocial.js
@@ -1,17 +1,41 @@
 function adaugaCazSocial() {
-	var numeCazSocial = document.getElementById('numeCazSocial').value.length;
-	var detaliiCazSocial = document.getElementById('detaliiCazSocial').value.length;
-	
-	if(numeCazSocial > 0 && detaliiCazSocial > 0 && $('#imageLoader').get(0).files.length !== 0){
-		toastr.info('Cazul social a fost adăugat cu succes!');
-		document.getElementById('numeCazSocial').value = "";
-		document.getElementById('detaliiCazSocial').value = "";
-		document.getElementById('imageLoader').value = "";
-		$('#modalAdaugaCazSocial').modal('hide');
-		return true;
-	}
-	else {
-		toastr.error('Completați câmpurile sau încărcați o poză.');
-		return false;
-	}
+    var numeCazSocial = document.getElementById('numeCazSocial').value.length;
+    var detaliiCazSocial = document.getElementById('detaliiCazSocial').value.length;
+
+    if (numeCazSocial > 0 && detaliiCazSocial > 0 && $('#imageLoader').get(0).files.length !== 0) {
+        toastr.info('Cazul social a fost adăugat cu succes!');
+        document.getElementById('numeCazSocial').value = "";
+        document.getElementById('detaliiCazSocial').value = "";
+        document.getElementById('imageLoader').value = "";
+        $('#modalAdaugaCazSocial').modal('hide');
+        return true;
+    }
+    else {
+        return false;
+    }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    document.getElementById('numeCazSocial').oninvalid = function (e) {
+        e.target.setCustomValidity("");
+        if (!e.target.validity.valid) {
+            e.target.setCustomValidity("Numele cazului trebuie introdus !");
+        }
+    };
+
+    document.getElementById('imageLoader').oninvalid = function (e) {
+        e.target.setCustomValidity("");
+        if (!e.target.validity.valid) {
+            e.target.setCustomValidity("Imaginea trebuie introdusă!");
+        }
+    };
+
+
+    document.getElementById('detaliiCazSocial').oninvalid = function (e) {
+        e.target.setCustomValidity("");
+        if (!e.target.validity.valid) {
+            e.target.setCustomValidity("Detaliile trebuie introduse !");
+        }
+    };
+});
--- js/login.js
+++ js/login.js
@@ -5,9 +5,9 @@ function validate() {
     var password = document.getElementById("password").value;
     if (username === "admin" && password === "admin") {
         window.location = "../admin/adminHomePage.html";
-    }else if(username.search("member") !== -1 && password.search("member") !== -1) {
-		window.location = "../member/memberHomePage.html"
-	}else {
+    } else if (username.search("member") !== -1 && password.search("member") !== -1) {
+        window.location = "../member/memberHomePage.html"
+    } else {
         attempt--;
         if ((username === "admin" && password !== "admin") || (username.search("member") !== -1 && password.search("member") === -1)) {
             alert("Parolă incorectă! Reîncercați!");
@@ -16,6 +16,8 @@ function validate() {
             alert("Utilizator incorect! Reîncercați!");
         }
         else {
+            //TODO : verify toastr, else add modal
+            // toastr.error("Utilizator si parolă incorecte!");
             alert("Utilizator si parolă incorecte!")
         }
         alert("You have left " + attempt + " attempt;");
